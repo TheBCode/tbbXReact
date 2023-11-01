6a845baec22bf83247c79d0e9ad86d27acfb206e
Byron Buyck <bbuyck1@gmail.com>
2023-11-01 13:00:09 -0400

    more edits...

---
+++ src/components/Project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Project = () => {
+    return (
+        <div className="container flex">
+            <div className="flex flex-row">Image</div>
+
+        </div>
+    )
+  
+}
